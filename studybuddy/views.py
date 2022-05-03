0e1bbd99bb842b77dd5a3fddf47b04eca15ece6f
uvaakn <akn7hgq@virginia.edu>
2022-05-03 13:11:08 -0400

    Event works except for writing
    :( idk

--- studybuddy/views.py
+++ studybuddy/views.py
@@ -22,9 +22,9 @@ from apiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 scopes = ['https://www.googleapis.com/auth/calendar']
 flow = InstalledAppFlow.from_client_secrets_file("credentials.json", scopes=scopes)
-credentials = flow.run_console()
+#credentials = flow.run_console()
 import pickle
-pickle.dump(credentials, open("token.pkl", "wb"))
+#pickle.dump(credentials, open("token.pkl", "wb"))
 credentials = pickle.load(open("token.pkl", "rb"))
 service = build("calendar", "v3", credentials=credentials)
 
@@ -95,10 +95,9 @@ def session(request):
             new_session.end_date = request.POST.get('end_date')
             new_session.save()
 
-            users = Profile.objects.filter().all()
             names = []
-            for member in new_session.users.all:
-                    emails.append(member.username)
+            for member in new_session.users.all():
+                    names.append(member.username)
             event = {
                 'summary': request.POST.get('subject') + " Study Session",
                 'location': request.POST.get('location'),
